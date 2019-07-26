73662777edce3f367490289a66e806731a1d6177
Sophiya <53294350+soiszzmael@users.noreply.github.com>
2019-07-26 09:52:18 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "SophiEGATIoT/"; //enter your appid
+		$KEY = "D0BMQshzhKMdAIp"; //enter your web key
+		$SECRET = "pkxyoIaNQdKefliKxSHKsA1PL"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
