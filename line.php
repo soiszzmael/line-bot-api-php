8b8a35c4bf6b60c7b6917bab18f0d9e3599bf04b
Sophiya <53294350+soiszzmael@users.noreply.github.com>
2019-07-26 09:34:41 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,8 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'lodTHUQpkT6KOjpGW++UWAOhE87vgzkH387vCXtHhx/VMyxxmQR/Vao3b4o+EO0bLN7xEX1x06SjuZ7bCaCVCYYETFDKAb4W+TIUCmLgvHH6cOyA28FPlzxv3wHczPai7cQb6v6LxE+ZC/RWrFc5wgdB04t89/1O/w1cDnyilFU=
+'; // Access Token ค่าที่เราสร้างขึ้น (Channel access token จาก LINE Developer)
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
