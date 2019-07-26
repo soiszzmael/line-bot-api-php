90e3510c0877088eb14b117a394a5789aa01acd0
Sophiya <53294350+soiszzmael@users.noreply.github.com>
2019-07-26 11:25:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$Topic = "ESP32_DHT11" ;			
+			$Topic = "SophiESP32" ;			
 			getMqttfromlineMsg($Topic,$text);   
 			
 		}
